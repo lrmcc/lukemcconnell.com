5ed7e6f1e105217505909351f0569a8751418928
Luke McConnell <lukemcconnell@Lukes-Mac-mini.local>
2021-04-12 00:24:05 -0600

    begining simplification and organization

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,16 +7,27 @@ function modeSelect(){
     let headerElement = document.getElementById("header");
     let moonElement = document.getElementById("moon");
     let sunElement = document.getElementById("sun");
+    let firstNameElement = document.getElementById("logo-first-name");
+    let lastNameElement = document.getElementById("logo-last-name");
+    let footerInfoElement = document.getElementById("footer-info")
+
+
     if (mode){
         mode = 0;
         moonElement.style.transform = "scale(0.5)";
         sunElement.style.transform = "scale(1)";
         headerElement.style.backgroundColor = "white";
+        firstNameElement.style.color= "#434242";
+        lastNameElement.style.color= "#434242";
+        footerInfoElement.style.color = "#434242";
     }
     else {
         mode = 1;
         moonElement.style.transform = "scale(1)";
         sunElement.style.transform = "scale(0.5)";
-        headerElement.style.backgroundColor = "#121212";
+        headerElement.style.backgroundColor = "#434242";
+        firstNameElement.style.color = "white";
+        lastNameElement.style.color = "white";
+        footerInfoElement.style.color = "white";
     }
 }
