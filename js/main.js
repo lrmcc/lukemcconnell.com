ba2107b2eb937612ebbf3c1142f5c7befa4f3dbc
Luke McConnell <lukemcconnell@Lukes-Mac-mini.local>
2021-08-16 23:19:24 -0600

    changed theme js based on CSS-Tricks. Parital update to content and colors.

--- js/main.js
+++ js/main.js
@@ -1,45 +1,21 @@
-let mode = 1;
+const btn = document.querySelector(".mode-toggle");
 
-function modeSelect(){
-    console.log("current mode: " + mode);
-    let i;
-    let bodyElement = document.body;
-    bodyElement.classList.toggle("body-light-mode");
-    let moonElement = document.getElementById("moon");
-    let sunElement = document.getElementById("sun");
-    let firstNameElement = document.getElementById("logo-first-name");
-    let lastNameElement = document.getElementById("logo-last-name");
-    let footerInfoElement = document.getElementById("footer-info")
-    let mainSectionNames = document.getElementsByClassName("main-section-name")
-    let sideSectionNames = document.getElementsByClassName("side-section-name")
-
-
-    if (mode){
-        mode = 0;
-        moonElement.style.transform = "scale(0.5)";
-        sunElement.style.transform = "scale(1)";
-        firstNameElement.style.color= "#434242";
-        lastNameElement.style.color= "#434242";
-        footerInfoElement.style.color = "#434242";
-        for (i = 0; i < mainSectionNames.length; i++) {
-            mainSectionNames[i].style.color = "#434242";
-            mainSectionNames[i].style.backgroundColor = "#d6d6d6";
-            sideSectionNames[i].style.color = "#434242";
-            sideSectionNames[i].style.backgroundColor = "#d6d6d6";
-        }
-    }
-    else {
-        mode = 1;
-        moonElement.style.transform = "scale(1)";
-        sunElement.style.transform = "scale(0.5)";
-        firstNameElement.style.color = "white";
-        lastNameElement.style.color = "white";
-        footerInfoElement.style.color = "white";
-        for (i = 0; i < mainSectionNames.length; i++) {
-            mainSectionNames[i].style.color = "white";
-            mainSectionNames[i].style.backgroundColor = "#5e5e5e";
-            sideSectionNames[i].style.color = "white";
-            sideSectionNames[i].style.backgroundColor = "#5e5e5e";
-        }
-    }
+// Get current "theme" from local storage, if equals "dark"
+// add to classList and therefore toggle CSS
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+  document.body.classList.add("dark-theme");
 }
+
+
+btn.addEventListener("click", function () {
+    // Toggle mode upon click
+    document.body.classList.toggle("dark-theme");
+    
+    // If dark-theme toggled set local storage "theme" to dark 
+    let theme = "light";
+    if (document.body.classList.contains("dark-theme")) {
+      theme = "dark";
+    }
+    localStorage.setItem("theme", theme);
+  });
